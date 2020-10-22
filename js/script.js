8b4ce9d3caf4f7ecf47daa741202dd7cb061ed5f
Mirelle Nascimento <mmirellenascimento@gmail.com>
2020-10-22 17:08:56 +0200

    JS part - 02

--- js/script.js
+++ js/script.js
@@ -2,6 +2,22 @@
 // var emailMessage = document.getElementById("message");
 var emailButton = document.getElementById("verificaButton");
 var message = "Non sei ancora registrato... Ma puoi giocare lo stesso ;)"
+var startButton = document.getElementById("iniziaPartita");
+var computerResult = document.getElementById("giocoComputer");
+var userResult = document.getElementById("giocoUser");
+var playButton = document.getElementById("gioca");
+var finalResult = document.getElementById("resultado");
+var restartButton = document.getElementById("reinizia");
+
+
+
+
+
+MIN_NUMBER = 1;
+MAX_NUMBER = 6;
+var computer = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER) + MIN_NUMBER);
+var player = Math.floor(Math.random() * (MAX_NUMBER - MIN_NUMBER) + MIN_NUMBER);
+
 
 var emailList = ["email1", "email2", "email3", "email4", "email5", "email6", "email7", "email8", "email9", "email10"];
 
@@ -19,5 +35,24 @@ emailButton.addEventListener("click", function(){
   }
   document.getElementById("message").innerHTML = message;
 
+  startButton.style.display = "block";
+});
 
+startButton.addEventListener("click", function(){
+  computerResult.innerHTML = computer;
+  playButton.style.display = "block";
+});
+
+playButton.addEventListener("click", function(){
+  userResult.innerHTML = player;
+
+  if (player > computer){
+    finalResult.innerHTML = "Hai vinto!";
+  } else if (player < computer){
+    finalResult.innerHTML = "Hai perso!";
+  } else {
+    finalResult.innerHTML = "È stato un pareggio!";
+  }
+
+  restartButton.style.display = "block";
 });
